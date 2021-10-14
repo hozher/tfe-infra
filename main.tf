7e3a4085be60ce0082ba129cde4ae8ebeedadc27
César <cesar.bonilla@bitso.com>
2021-10-14 15:13:17 -0500

    Update: add data

--- main.tf
+++ main.tf
@@ -5,10 +5,14 @@ provider "aws" {
   }
 }
 
+data "aws_secretsmanager_secret" "tfe_license" {
+  name = "tfe_license"
+}
+
 module "tfe_node" {
   source               = "./terraform/aws-tfe"
   friendly_name_prefix = var.friendly_name_prefix
   domain_name          = var.domain_name
-  tfe_license_secret   = "dummy.rli"
+  tfe_license_secret   = data.aws_secretsmanager_secret.tfe_license
   acm_certificate_arn  = var.acm_certificate_arn
 }
